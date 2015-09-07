4c42e79dde844fc99a3c88e58b0ee7f65af7d218
Luke Van Seters <lukevanseters@gmail.com>
2015-09-07 11:28:08 -0400

    Wrap picswap in a class for use beyond the command line

--- picswap.py
+++ picswap.py
@@ -8,39 +8,73 @@ from os import path
 from scipy import misc
 from skimage import color
 
-def argsort_by_value(arr):
-    """
-    Argsorts the array by HSV value.
+class PicSwapper(object):
+    """class for rearranging one image to look like another"""
 
-    :param arr: image array to argsort
-    :type arr: numpy.ndarray
-    :returns: linearized indices to sort arr
-    """
+    def __init__(self): pass
 
-    as_hsv = color.rgb2hsv(arr.reshape(1, -1, 3))
-    values = as_hsv[:,:,2]
-    return np.argsort(values.reshape(-1))
+    def argsort_by_value(self, arr):
+        """Argsorts the array by HSV value
 
-def swap(first, second, argsort=argsort_by_value):
-    """
-    Rearranges the pixels in one image to create resemble another.
+        :param arr: image array to argsort
+        :type arr: numpy.ndarray
 
-    :param first: image to rearrange
-    :type first: numpy.ndarray
-    :param second: image to rearrange into
-    :type second: numpy.ndarry
-    :param argsort: function to argsort an image
-    :type argsort: function
-    :returns: the rearranged image
-    """
+        :returns: linearized indices to sort arr
+        :rtype: numpy.ndarray
+        """
 
-    # sort first
-    first = first.reshape(-1, 3)[argsort(first)]
+        as_hsv = color.rgb2hsv(arr.reshape(1, -1, 3))
+        values = as_hsv[:,:,2]
+        return np.argsort(values.reshape(-1))
 
-    # reverse argsort second
-    rearranged = argsort(second).argsort()
+    def swap(self, first, second, argsort=None):
+        """Rearranges the pixels in one image to create resemble another
 
-    return first[rearranged].reshape(second.shape)
+        :param first: image to rearrange
+        :type first: numpy.ndarray
+
+        :param second: image to rearrange into
+        :type second: numpy.ndarray
+
+        :param argsort: function to argsort an image
+        :type argsort: function
+
+        :returns: the rearranged image
+        :rtype: numpy.ndarray
+        """
+
+        # set argsort
+        if argsort is None:
+            argsort = self.argsort_by_value
+
+        # scale first if necessary
+        first = self.concatenate_to_scale(first, second.size)
+
+        # sort first
+        first = first.reshape(-1, 3)[argsort(first)]
+
+        # reverse argsort second
+        rearranged = argsort(second).argsort()
+
+        return first[rearranged].reshape(second.shape)
+
+    def concatenate_to_scale(self, arr, size):
+        """Concatenates an array onto itself until it is at least size
+
+        :param arr: arr to scale
+        :type arr: numpy.ndarray
+
+        :param size: size to scale to or above
+        :type size: int
+
+        :returns: the scaled array
+        :rtype: numpy.ndarray
+        """
+
+        while arr.size < size:
+            arr = np.concatenate((arr, arr))
+
+        return arr
 
 def main(argv):
 
@@ -58,14 +92,11 @@ def main(argv):
     print("opening")
     first = misc.imread(firstpath)
     second = misc.imread(secondpath)
-    
-    # handle image too small
-    while first.size < second.size:
-        first = np.concatenate((first, first))
 
     # swap
     print("swapping")
-    img = swap(first, second)
+    ps = PicSwapper()
+    img = ps.swap(first, second)
 
     # save
     print ("saving")
